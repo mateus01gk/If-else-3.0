1260096ebbbfe08f435d94266000634d678d778b
mateus01gk <169069087+mateus01gk@users.noreply.github.com>
2024-09-02 12:20:03 +0000

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    System.out.println("digite seu login");
+    double login = sc.nextDouble();
+    double senhaAdmin = 12345;
+
+
+
+
+
+
+    sc.close();
+  }
+
+  
+}
